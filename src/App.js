59f1570d10860e7fac1b0d83ce8a94bd39135ff5
BogdanShtokhmal <sht.bogdan@gmail.com>
2020-12-28 19:55:31 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useReducer}  from 'react';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const reducer = (state, action)=>{
+switch (action.type){
+    case 'SET_TODO_ITEM':{
+      return action.payload;
+    }
+    case "RESET_TODO":{
+        return null;
+    }
+    case "TOGLE_TODO_COMPLETED":{
+        return {
+            ...state,
+            completed: !state.completed
+        };
+    }
+    case "CHANGE_TODO_TITLE":{
+        return {
+            ...state,
+            title: action.payload
+        };
+    }
+    default:{
+        console.error('not valid action', action.type);
+        return  state;
+    }
+}
+};
+
+const initialState = null;
+
+export default function App(){
+const [counter, setCounter] = useState(1);
+// const [todo, setTodo] = useState(null);
+const [state, dispatch] = useReducer(reducer, initialState);
+
+const [inputValue, inputChange] = useState('');
+
+
+const fetchData = async ()=>{
+    const response= await fetch(`https://jsonplaceholder.typicode.com/todos/${counter}`);
+    const json=await response.json();
+    dispatch({type:'SET_TODO_ITEM',payload: json});
+
 }
 
-export default App;
+useEffect(()=>{
+
+    console.log('i was called');
+
+    fetchData();
+
+
+
+},[counter]);
+
+    const hendleInc=()=> {setCounter((prevState) => prevState+1)};
+    const hendleReset=()=> setCounter(1);
+
+    const hendleResetTodo=()=> dispatch({type:"RESET_TODO"});
+
+    const hendleTodoCompleted=()=> dispatch({type:"TOGLE_TODO_COMPLETED"})
+
+    const handlerTitleChange=()=>{
+    dispatch({type:"CHANGE_TODO_TITLE", payload:inputValue});
+
+    };
+
+
+
+
+  return (
+      <div className="App">
+        <h1>Counter: {counter}</h1>
+
+          <button onClick={hendleInc}>Inc</button>
+          {/*<button onClick={hendleReset}>Reset</button>*/}
+          <button onClick={hendleResetTodo}>Reset_todo</button>
+          <button onClick={hendleTodoCompleted}>togle</button>
+          <input
+              value={inputValue}
+              onChange={({target:{value}})=>inputChange(value)}/>
+          <button onClick={handlerTitleChange}>change title</button>
+          {!!state && <h2>
+              {state.id}-{state.title}-{state.completed.toString()}
+          </h2>}
+
+      </div>
+  );
+
+
+}
+
+
+
+
+
+
+
+
